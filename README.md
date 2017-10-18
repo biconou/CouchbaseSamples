0a53035f531cd9d4f68d1275e04ddcab6d80458c
Rémi Cocula <rcocula@gmail.com>
2017-10-18 21:47:02 +0200

    WIP

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Installation rapide avec Docker
 https://developer.couchbase.com/documentation/server/4.6/getting-started/do-a-quick-install.html
 
-docker run -t --name db -p 8091-8094:8091-8094 -p 11210:11210 couchbase/server:sandbox
+docker run -t --name couchbase-sandbox -p 8091-8094:8091-8094 -p 11210:11210 -d couchbase/server:sandbox
+
 
 Documentation sur les images docker : https://github.com/couchbase-guides/couchbase-docker
 
--- src/test/java/com/github/biconou/couchbase/samples/SimpleN1QLTest.java
+++ src/test/java/com/github/biconou/couchbase/samples/SimpleN1QLTest.java
@@ -1,14 +1,28 @@
 package com.github.biconou.couchbase.samples;
 
+import com.couchbase.client.deps.com.fasterxml.jackson.core.JsonParser;
+import com.couchbase.client.deps.com.fasterxml.jackson.core.type.TypeReference;
+import com.couchbase.client.deps.com.fasterxml.jackson.databind.ObjectMapper;
+import com.couchbase.client.deps.com.fasterxml.jackson.databind.ObjectReader;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.json.JsonArray;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.env.CouchbaseEnvironment;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
 import com.couchbase.client.java.query.N1qlQuery;
 import com.couchbase.client.java.query.N1qlQueryResult;
 import com.couchbase.client.java.query.N1qlQueryRow;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class SimpleN1QLTest {
 
@@ -18,8 +32,9 @@ public class SimpleN1QLTest {
 
     @BeforeClass
     public static void setup() {
-        cluster = CouchbaseCluster.create("localhost");
-        bucket = cluster.openBucket("beer-sample");
+        CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder().connectTimeout(10000).build();
+        cluster = CouchbaseCluster.create(env,"localhost");
+        bucket = cluster.openBucket("travel-sample");
 
         // Create a N1QL Primary Index (but ignore if it exists)
         bucket.bucketManager().createN1qlPrimaryIndex(true, false);
@@ -42,18 +57,18 @@ public class SimpleN1QLTest {
     }
 
     @Test
-    public void listDistinctCountries() {
+    public void listDistinctTypes() {
         // Perform a N1QL Query
         N1qlQueryResult result = bucket.query(
-                N1qlQuery.parameterized("SELECT distinct country FROM `beer-sample` where type = $1",
-                        JsonArray.from("brewery"))
-        );
+                N1qlQuery.simple("SELECT distinct type FROM `travel-sample`"));
 
         // Print each found Row
-        for (N1qlQueryRow row : result) {
-            System.out.println(row.value());
-        }
-        System.out.println(result.info().resultCount());
+       List<String> types = StreamSupport.stream(result.spliterator(),false)
+                .map(p -> p.value().getString("type"))
+                .collect(Collectors.toList());
+
+
+         types.forEach(System.out::println);
     }
 
 }
